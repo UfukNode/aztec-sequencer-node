845aba92d0d3b12ae04878d2635367d5d6c84c30
ufukdegen <ufuk.node@gmail.com>
2025-05-26 00:53:26 +0300

    Update validator_kayit.sh

--- validator_kayit.sh
+++ validator_kayit.sh
@@ -7,8 +7,8 @@ RESET='\033[0m'
 echo -e "${ORANGE}Validator olarak kayıt olunuyor...${RESET}"
 
 read -p "Sepolia RPC URL girin: " RPC
-read -p "Cüzdan Private Key girin: " PRVKEY
-read -p "Cüzdan Adresi: " PUBKEY
+read -p "Cüzdan Private Keyinizi girin: " PRVKEY
+read -p "Cüzdan Adresinizi Girin: " PUBKEY
 
 OUTPUT=$(aztec add-l1-validator \
   --l1-rpc-urls $RPC \
@@ -20,7 +20,7 @@ OUTPUT=$(aztec add-l1-validator \
 
 if echo "$OUTPUT" | grep -qi "ValidatorQuotaFilledUntil"; then
   TS=$(echo "$OUTPUT" | grep -oE 'ValidatorQuotaFilledUntil\([0-9]+\)' | grep -oE '[0-9]+' | head -n1)
-  HUMAN_TIME=$(date -d "@$TS" "+%d %B %Y %H:%M:%S %Z" TZ="Europe/Istanbul")
+  HUMAN_TIME=$(TZ="Europe/Istanbul" date -d "@$TS" "+%d %B %Y %H:%M:%S %Z")
   echo -e "${ORANGE}⚠ Günlük validator limiti dolmuş olabilir.${RESET}"
   echo -e "${GREEN}⏳ Bir sonraki deneme zamanı (TR saatiyle): $HUMAN_TIME${RESET}"
 elif echo "$OUTPUT" | grep -qi "Error\|invalid\|stack"; then
